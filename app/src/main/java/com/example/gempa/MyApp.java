9d60b1f155ffe27b3def7ea57e028fefcc044ded
yantiyu65 <Bowyanti4@gmail.com>
2025-06-02 16:17:10 +0700

    commit gempa

---
+++ app/src/main/java/com/example/gempa/MyApp.java
@@ -0,0 +1,23 @@
+package com.example.gempa;
+
+
+import android.app.Application;
+import android.content.Context;
+
+import com.onesignal.OneSignal;
+
+public class MyApp extends Application {
+    Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+
+        // Inisialisasi OneSignal
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId("f0b98772-3ee0-4809-a26f-a52530b068ef");
+
+
+
+    }
+}
